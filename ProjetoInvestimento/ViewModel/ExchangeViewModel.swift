33556bb75a1b1a93fe47bf1b569334e33b99894e
setoue <proshigeru@gmail.com>
2022-11-14 14:40:59 -0300

    removed var's underlines and void spaces

--- ProjetoInvestimento/ViewModel/ExchangeViewModel.swift
+++ ProjetoInvestimento/ViewModel/ExchangeViewModel.swift
@@ -26,28 +26,28 @@ class ExchangeViewModel {
         
         self.service.getExchangeFromAPI { success, error in
             
-            if var _success = success {
+            if var success = success {
                 print("O obejto está sendo retornado")
                 
-                _success.results.currencies.ARS.sigla = "ARS"
-                _success.results.currencies.USD.sigla = "USD"
-                _success.results.currencies.EUR.sigla = "EUR"
-                _success.results.currencies.AUD.sigla = "AUD"
-                _success.results.currencies.GBP.sigla = "GBP"
-                _success.results.currencies.CAD.sigla = "CAD"
-                _success.results.currencies.JPY.sigla = "JPY"
-                _success.results.currencies.BTC.sigla = "BTC"
-                _success.results.currencies.CNY.sigla = "CNY"
+                success.results.currencies.ARS.sigla = "ARS"
+                success.results.currencies.USD.sigla = "USD"
+                success.results.currencies.EUR.sigla = "EUR"
+                success.results.currencies.AUD.sigla = "AUD"
+                success.results.currencies.GBP.sigla = "GBP"
+                success.results.currencies.CAD.sigla = "CAD"
+                success.results.currencies.JPY.sigla = "JPY"
+                success.results.currencies.BTC.sigla = "BTC"
+                success.results.currencies.CNY.sigla = "CNY"
                 
-                self.allCoins.append(_success.results.currencies.USD)
-                self.allCoins.append(_success.results.currencies.EUR)
-                self.allCoins.append(_success.results.currencies.ARS)
-                self.allCoins.append(_success.results.currencies.AUD)
-                self.allCoins.append(_success.results.currencies.GBP)
-                self.allCoins.append(_success.results.currencies.CAD)
-                self.allCoins.append(_success.results.currencies.JPY)
-                self.allCoins.append(_success.results.currencies.BTC)
-                self.allCoins.append(_success.results.currencies.CNY)
+                self.allCoins.append(success.results.currencies.USD)
+                self.allCoins.append(success.results.currencies.EUR)
+                self.allCoins.append(success.results.currencies.ARS)
+                self.allCoins.append(success.results.currencies.AUD)
+                self.allCoins.append(success.results.currencies.GBP)
+                self.allCoins.append(success.results.currencies.CAD)
+                self.allCoins.append(success.results.currencies.JPY)
+                self.allCoins.append(success.results.currencies.BTC)
+                self.allCoins.append(success.results.currencies.CNY)
                 
                 self.delegate?.success()
                 
@@ -65,18 +65,18 @@ class ExchangeViewModel {
         return self.allCoins[indexPath].sigla ?? ""
     }
     public func returnVariationPercent(indexPath: Int) -> String{
-        let _variation = String(self.allCoins[indexPath].variation)
-        return "\(_variation)%"
+        let variation = String(self.allCoins[indexPath].variation)
+        return "\(variation)%"
     }
     
     public func returnVariationColor(indexPath: Int) -> UIColor {
         
-        let _variation = self.allCoins[indexPath].variation
+        let variation = self.allCoins[indexPath].variation
     
-        if _variation > 0 {
+        if variation > 0 {
             return UIColor.green
             
-        } else if _variation < 0 {
+        } else if variation < 0 {
             return UIColor.red
             
         } else {
@@ -85,14 +85,14 @@ class ExchangeViewModel {
     }
     
     public func returnSigla(indexPath: Int) -> String{
-        let _sigla = self.allCoins[indexPath].sigla ?? nil
-        guard let sigla = _sigla else { return ""}
+        let sigla = self.allCoins[indexPath].sigla ?? nil
+        guard let sigla = sigla else { return ""}
         
         return sigla
     }
     public func returnCoin(indexPath: Int) -> String{
-        let _name = self.allCoins[indexPath].name
-        return _name
+        let name = self.allCoins[indexPath].name
+        return name
     }
     
     public func returnCompra(indexPath: Int) -> Double{
@@ -102,7 +102,5 @@ class ExchangeViewModel {
     public func returnVenda(indexPath: Int) -> Double{
         guard let venda = self.allCoins[indexPath].sell else { return 0}
         return venda
-    }
-
-    
+    }   
 }
